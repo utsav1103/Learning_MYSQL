0b4673b96ff5a41f6cb74f3d22ea1a472094b3c4
utsav-baghela <utsavu858@gmail.com>
2024-01-11 17:44:16 +0530

    Introducing CRUD

--- pet_store.sql
+++ pet_store.sql
@@ -1,2 +1,23 @@
--- Active: 1703925228325@@127.0.0.1@3306@pet_store
-SELECT * FROM dogs;
+-- Active: 1703925228325@@127.0.0.1@3306@pet_shop
+
+SHOW TABLES;
+DROP table cats;
+SHOW TABLES;
+CREATE TABLE cats (
+    cat_id INT AUTO_INCREMENT,
+    name VARCHAR(20),
+    breed VARCHAR(20),
+    age INT,
+    PRIMARY KEY (cat_id)
+);
+DESC cats;
+INSERT INTO cats(name, breed, age)
+VALUES ('R', 'ta', 4),
+('C', 'mc', 34),
+('D', 'mc', 23),
+('E', 'per', 4),
+('M', 'ta',2),
+('G', 'Ra', 7),
+('J', 'sa', 6);
+SELECT * FROM cats;
+ 
