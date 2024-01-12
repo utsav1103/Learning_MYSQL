5b4716439d6e4814dacbdb267852199bb5c9997a
utsav-baghela <utsavu858@gmail.com>
2024-01-12 19:39:28 +0530

    introducing where

--- pet_shop.sql
+++ pet_shop.sql
@@ -1,6 +1,4 @@
 -- Active: 1703925228325@@127.0.0.1@3306@pet_shop
-SELECT * FROM cats;
-SELECT name FROM cats;
-SELECT age FROM cats;
-SELECT name, age FROM cats;
-SELECT age, breed FROM cats;
+SELECT * FROM cats WHERE age = 4;
+SELECT name FROM cats WHERE age = 4;
+SELECT * FROM cats WHERE name = 'E';
