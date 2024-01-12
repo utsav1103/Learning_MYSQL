0f2d1f485f64e35ac80f90d8fe1bddb5d6c428a2
utsav-baghela <utsavu858@gmail.com>
2024-01-12 19:51:39 +0530

    using update

--- pet_shop.sql
+++ pet_shop.sql
@@ -1,4 +1,6 @@
 -- Active: 1703925228325@@127.0.0.1@3306@pet_shop
-SELECT * FROM cats WHERE age = 4;
-SELECT name FROM cats WHERE age = 4;
-SELECT * FROM cats WHERE name = 'E';
+SELECT * FROM employees; 
+UPDATE employees SET last_name= 'rooster';
+SELECT * FROM employees;
+UPDATE employees SET currnet_status='laid-off';
+SELECT * FROM employees;
