094ffc7b13d68eaa3473a76955bed45cc550adcf
utsav-baghela <utsavu858@gmail.com>
2024-01-13 18:13:36 +0530

    introducing DELETE

--- pet_shop.sql
+++ pet_shop.sql
@@ -1,7 +1,6 @@
 -- Active: 1703925228325@@127.0.0.1@3306@pet_shop
-SELECT * FROM cats WHERE name ='J';
-UPDATE cats SET name='JA' WHERE name='J';
-SELECT * FROM cats;
-UPDATE cats SET age= 12 WHERE breed='mc';
-SELECT * FROM cats;
+DELETE FROM cats WHERE name='E';
 
+SELECT * FROM cats;
+SELECT * FROM employees;
+DELETE FROM employees;
