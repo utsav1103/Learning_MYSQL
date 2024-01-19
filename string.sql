4d9550d545057a2ce514d6890e3b54e6c8801432
utsav-baghela <utsavu858@gmail.com>
2024-01-19 14:13:32 +0530

    combining String functions

---
+++ string.sql
@@ -0,0 +1,7 @@
+-- -- Active: 1703925228325@@127.0.0.1@3306@book_shop
+SELECT * FROM books;
+
+SELECT CONCAT(SUBSTR(title, 1, 10), '...') AS short_title FROM books;
+
+SELECT CONCAT(SUBSTR(author_fname, 1, 1),'.', SUBSTR(author_lname, 1, 1), '.') AS author_initials
+FROM books;
