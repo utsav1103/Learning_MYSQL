027fa165e7d452bdb81b1d3cb9f8474a2c425fee
utsav-baghela <utsavu858@gmail.com>
2024-01-25 18:02:52 +0530

    introducing Replace

--- string.sql
+++ string.sql
@@ -1,7 +1,20 @@
 -- -- Active: 1703925228325@@127.0.0.1@3306@book_shop
-SELECT * FROM books;
+SELECT
+    *
+FROM
+    books;
 
-SELECT CONCAT(SUBSTR(title, 1, 10), '...') AS short_title FROM books;
+SELECT
+    CONCAT(SUBSTR(title, 1, 10), '...') AS short_title
+FROM
+    books;
 
-SELECT CONCAT(SUBSTR(author_fname, 1, 1),'.', SUBSTR(author_lname, 1, 1), '.') AS author_initials
-FROM books;
+SELECT
+    CONCAT(
+        SUBSTR(author_fname, 1, 1),
+        '.',
+        SUBSTR(author_lname, 1, 1),
+        '.'
+    ) AS author_initials
+FROM
+    books;
---
+++ string1.sql
@@ -0,0 +1,4 @@
+-- -- Active: 1703925228325@@127.0.0.1@3306@book_shop
+SELECT * FROM books;
+SELECT REPLACE(title, ' ', '-') FROM books;
+SELECT title FROM books; 
