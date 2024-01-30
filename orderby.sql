30bb10a89ec55c0d62b4de5aeeae45c1fbfeffcb
utsav-baghela <utsavu858@gmail.com>
2024-01-30 15:04:56 +0530

    More on order by

--- orderby.sql
+++ orderby.sql
@@ -5,3 +5,11 @@ SELECT title, pages FROM books;
 SELECT title , pages FROM books ORDER BY pages;
 SELECT title, pages FROM books ORDER BY released_year;
 SELECT title, pages, released_year FROM books ORDER BY released_year;
+-- more on order by 
+SELECT book_id, author_fname, author_lname, pages FROM books ORDER BY 4;
+SELECT book_id, author_fname, author_lname, pages FROM books ORDER BY 4 DESC;
+SELECT author_lname , released_year, title FROM books;
+SELECT author_lname , released_year, title FROM books ORDER BY author_lname, released_year;
+SELECT CONCAT(author_fname,' ', author_lname) AS author FROM books;
+
+SELECT CONCAT(author_fname,' ', author_lname) AS author FROM books ORDER BY author;
