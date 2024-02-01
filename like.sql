3ccafebbb0357465dc2dd18a538e896f13557334
utsav-baghela <utsavu858@gmail.com>
2024-02-01 16:36:50 +0530

    some more like

--- like.sql
+++ like.sql
@@ -3,7 +3,10 @@ SELECT title, author_fname , author_lname FROM books WHERE author_fname='David';
 SELECT title, author_fname , author_lname FROM books WHERE author_fname LIKE '%da%';
 SELECT * FROM books WHERE title LIKE '%:%';
 SELECT * FROM books;
+
 SELECT * FROM books WHERE author_fname LIKE '____';
 SELECT * FROM books WHERE author_fname LIKE '_a_';
 SELECT * FROM books WHERE author_fname LIKE '%a%';
 SELECT * FROM books WHERE author_fname LIKE '%n';
+SELECT * FROM books WHERE title LIKE '%\%%';
+SELECT * FROM books WHERE title LIKE '%\_%';
