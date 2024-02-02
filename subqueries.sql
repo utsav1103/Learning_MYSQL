73be3457fcae009b93d04657070d881c42e8a61a
utsav-baghela <utsavu858@gmail.com>
2024-02-02 19:21:28 +0530

    learning subqueries

---
+++ subqueries.sql
@@ -0,0 +1,8 @@
+-- -- Active: 1703925228325@@127.0.0.1@3306@book_shop
+SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+-- subqueries method
+SELECT title, pages FROM books WHERE pages = (SELECT MAX(pages)FROM books);
+INSERT INTO books (title , pages) VALUES('my life in words', 634);
+
+SELECT title, released_year FROM books
+WHERE released_year = (SELECT MIN(released_year)FROM books);
