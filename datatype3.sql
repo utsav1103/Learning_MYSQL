ed00afd242a2315ce20cd246f765d509e5374281
utsav-baghela <utsavu858@gmail.com>
2024-02-06 14:40:51 +0530

    using date,time & datetime

--- datatype3.sql
+++ datatype3.sql
@@ -1,7 +1,13 @@
- -- -- Active: 1703925228325@@127.0.0.1@3306@book_shop
- -- introducing to date & TIME
- --'YYYY-MM-DD' TIME FORMAT
-
-
- -- DATETIME FORMAT'YYYY-MM-DD HH:MM:SS'
-
+-- -- Active: 1703925228325@@127.0.0.1@3306@book_shop
+-- introducing to date & TIME
+--'YYYY-MM-DD' TIME FORMAT
+-- DATETIME FORMAT'YYYY-MM-DD HH:MM:SS'
+CREATE Table people(
+    name VARCHAR(100),
+    birthdate DATE,
+    birthtime TIME,
+    birthdt DATETIME
+);
+DESC people;
+INSERT INTO people(name, birthdate,birthtime,birthdt) VALUES('Elton','2000-12-25','11:00:00','2000-12-25 11:00:00');
+SELECT * FROM people;
