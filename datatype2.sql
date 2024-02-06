9eaf1b9bbc4b69059eabf33eb21296eb772122b9
utsav-baghela <utsavu858@gmail.com>
2024-02-06 14:12:55 +0530

    using decimal

---
+++ datatype2.sql
@@ -0,0 +1,16 @@
+ -- -- Active: 1703925228325@@127.0.0.1@3306@book_shop
+SELECT * FROM parent;
+INSERT INTO parent(children) VALUES (90.0002);
+-- ROUNDUP value is STORED 
+CREATE Table products(price DECIMAL(5,2));
+
+INSERT INTO products(price) VALUES(4.30);
+
+SELECT * FROM products;
+
+INSERT INTO products(price) VALUES(534.32);
+
+
+INSERT INTO products(price) VALUES(5.3432);
+
+SELECT * FROM products;
