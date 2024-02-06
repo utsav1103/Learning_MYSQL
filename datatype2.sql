f30d13590c2b46cab6053a2b5ed5df5c06194212
utsav-baghela <utsavu858@gmail.com>
2024-02-06 14:26:02 +0530

    floar & double

--- datatype2.sql
+++ datatype2.sql
@@ -14,3 +14,12 @@ INSERT INTO products(price) VALUES(534.32);
 INSERT INTO products(price) VALUES(5.3432);
 
 SELECT * FROM products;
+
+-- FLOAT use 4 bytes and cause precision issues after ~ 7 digits and DOUBLE use 8 bytes and cause precision(exactness) issues after ~15 digits
+ CREATE Table numbs(x FLOAT, y DOUBLE);
+ INSERT INTO numbs(x,y) VALUES(1.123, 1.123);
+
+ SELECT * FROM numbs;
+ INSERT INTO numbs(x,y) VALUES(1.12345678, 1.12345678);
+ SELECT * FROM numbs;
+ 
