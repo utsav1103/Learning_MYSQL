68b5d1c528e59caa4ab07376f7dd3616a6cde811
utsav-baghela <utsavu858@gmail.com>
2024-02-12 19:39:23 +0530

    lessthan and equal to

--- greaterthan.sql
+++ greaterthan.sql
@@ -4,3 +4,8 @@ SELECT * FROM books WHERE pages > 500;
 SELECT 80 > 45;
 SELECT 30 > 100;
 SELECT title , pages  , released_year FROM books WHERE pages > 500;
+-- LESS THAN and equals TO
+SELECT title, released_year FROM books WHERE released_year < 2000 ORDER BY released_year ;
+
+SELECT title, pages FROM books WHERE pages < 200;
+SELECT  title, released_year FROM books WHERE released_year >= 2010;
