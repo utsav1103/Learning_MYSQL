dc999bb2c94455ef4c5c96c6140ca57134006edd
utsav-baghela <utsavu858@gmail.com>
2024-02-23 14:03:29 +0530

    cross joins in sql

---
+++ Crossjoint.sql
@@ -0,0 +1,10 @@
+-- -- Active: 1703925228325@@127.0.0.1@3306@shop
+
+
+SELECT id FROM customers WHERE last_name ='George';
+
+SELECT * FROM orders WHERE customer_id = 1;
+
+SELECT * FROM orders WHERE customer_id =(SELECT id FROM customers WHERE last_name = 'George');
+
+SELECT * FROM customers,orders;
