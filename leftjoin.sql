efe976cea1eee0d3588460011672d734827854e8
utsav-baghela <utsavu858@gmail.com>
2024-02-23 20:52:36 +0530

    Left Join with group by

--- leftjoin.sql
+++ leftjoin.sql
@@ -1,5 +1,25 @@
 -- -- Active: 1703925228325@@127.0.0.1@3306@shop
+SELECT
+    first_name,
+    last_name,
+    order_date,
+    amount
+FROM
+    customers
+    LEFT JOIN orders ON orders.customer_id = customers.id;
 
-SELECT first_name, last_name, order_date, amount FROM customers LEFT JOIN orders ON orders.customer_id = customers.id;
+SELECT
+    *
+FROM
+    customers;
 
-SELECT * FROM customers;
+SELECT
+    first_name,
+    last_name,
+    IFNULL(SUM(amount), 0) as money_spent
+FROM
+    customers
+    LEFT JOIN orders on customers.id = orders.customer_id
+GROUP BY
+    first_name,
+    last_name;
