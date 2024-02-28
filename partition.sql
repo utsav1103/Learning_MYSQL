23a92eca2fbf314dd5e41b169916a8429684f10b
utsav-baghela <utsavu858@gmail.com>
2024-02-28 22:06:56 +0530

    using partition by window function

---
+++ partition.sql
@@ -0,0 +1,24 @@
+-- -- Active: 1703925228325@@127.0.0.1@3306@tv_db
+SELECT
+    emp_no,
+    department,
+    salary,
+    AVG(salary) OVER(PARTITION BY department) as dept_avg
+FROM
+    employees;
+
+SELECT
+    emp_no,
+    department,
+    salary,
+    COUNT(*) OVER(PARTITION BY department) as dept_count
+FROM
+    employees;
+    SELECT
+    emp_no,
+    department,
+    salary,
+    SUM(salary) OVER(PARTITION BY department) as dept_payroll,
+    SUM(salary) OVER() as total_payroll
+FROM
+    employees;
