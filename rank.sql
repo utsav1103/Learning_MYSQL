83d0dec8f433aa4ce58fcc4f47c2b4773ff1e0ad
utsav-baghela <utsavu858@gmail.com>
2024-02-29 19:54:46 +0530

    dense_rank & row_numbers

--- rank.sql
+++ rank.sql
@@ -3,10 +3,46 @@ SELECT
     emp_no,
     department,
     salary,
-    RANK() OVER(PARTITION BY department ORDER BY salary DESC) as dept_salary_rank,
+    RANK() OVER(
+        PARTITION BY department
+        ORDER BY
+            salary DESC
+    ) as dept_salary_rank,
     RANK() OVER(
         ORDER BY
             salary DESC
     ) as overall_salary_rank
 FROM
     employees;
+
+-- dense RANK and ROW NUMBER
+SELECT
+    emp_no,
+    department,
+    salary,
+    ROW_NUMBER() OVER(
+        PARTITION BY department
+        ORDER BY
+            salary DESC
+    ) as dept_row_number,
+    RANK() OVER(
+        PARTITION BY department
+        ORDER BY
+            salary DESC
+    ) as dept_salary_rank,
+    RANK() OVER(
+        ORDER BY
+            salary DESC
+    ) as overall_salary_rank,
+    DENSE_RANK() OVER(
+        ORDER BY
+            salary DESC
+    ) as dense_salary_rank,
+    ROW_NUMBER() OVER(
+        ORDER BY
+            salary DESC
+    ) as overall_number
+FROM
+    employees
+ORDER BY
+    overall_salary_rank;
