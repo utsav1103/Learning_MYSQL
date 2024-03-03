a2b7d87efa261e16958ba1acc333fc02c9ea00fe
utsav-baghela <utsavu858@gmail.com>
2024-03-03 18:29:24 +0530

    LIKES schema

--- instagram/ig_clone.sql
+++ instagram/ig_clone.sql
@@ -12,15 +12,16 @@ DESC users;
 INSERT INTO
     users(username)
 VALUES
-('blue'),
-('charli'),
-('colt');
+    ('blue'),
+    ('charli'),
+    ('colt');
 
 SELECT
     *
 FROM
     users;
 
+-- CREATEING photos SCHEMA
 CREATE Table photos(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     image_url VARCHAR(255) NOT NULL,
@@ -28,11 +29,47 @@ CREATE Table photos(
     created_at TIMESTAMP DEFAULT now(),
     Foreign Key (user_id) REFERENCES users (id)
 );
+
 SHOW TABLES;
+
 DESC photos;
 
-INSERT INTO photos(image_url, user_id) VALUES('/dhjdfh',1),('/uehosfhl', 2),('/iuurth', 2);
+INSERT INTO
+    photos(image_url, user_id)
+VALUES
+    ('/dhjdfh', 1),
+    ('/uehosfhl', 2),
+    ('/iuurth', 2);
 
-SELECT photos.image_url, users.username
-FROM photos
-JOIN users ON photos.user_id = users.id;
+SELECT
+    photos.image_url,
+    users.username
+FROM
+    photos
+    JOIN users ON photos.user_id = users.id;
+
+-- CREATEING COMMENTS SCHEMA
+CREATE TABLE comments(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    Foreign Key (user_id) REFERENCES users (id),
+    Foreign Key (photo_id) REFERENCES photos (id)
+);
+
+show TABLES;
+
+DESC comments;
+
+-- LIKES SCHEMA
+CREATE Table likes(
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    Foreign Key (user_id) REFERENCES users (id),
+    Foreign Key (photo_id) REFERENCES photos (id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
