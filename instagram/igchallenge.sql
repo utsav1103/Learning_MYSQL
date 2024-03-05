5ec8ee2532963c484ed180a65bb50aecef6fe47c
utsav-baghela <utsavu858@gmail.com>
2024-03-05 15:54:03 +0530

    igchallenge 2

--- instagram/igchallenge.sql
+++ instagram/igchallenge.sql
@@ -3,3 +3,8 @@
 -- Instagram challenge #1
 
 SELECT * FROM users ORDER BY created_at LIMIT 5;
+
+-- Instagram challenge #2
+
+SELECT DAYNAME(created_at)as day ,
+COUNT(*) as total  FROM users GROUP BY day ORDER BY total DESC LIMIT 2;
