b70e8df730b7c3703f78c1c4bd02b2b17a6601f2
utsav-baghela <utsavu858@gmail.com>
2024-03-08 19:26:44 +0530

    ig challenge #4

--- instagram/igchallenge.sql
+++ instagram/igchallenge.sql
@@ -1,15 +1,50 @@
 -- -- Active: 1703925228325@@127.0.0.1@3306@ig_clone
-
 -- Instagram challenge #1
-
-SELECT * FROM users ORDER BY created_at LIMIT 5;
+SELECT
+    *
+FROM
+    users
+ORDER BY
+    created_at
+LIMIT
+    5;
 
 -- Instagram challenge #2
-
-SELECT DAYNAME(created_at)as day ,
-COUNT(*) as total  FROM users GROUP BY day ORDER BY total DESC LIMIT 2;
+SELECT
+    DAYNAME(created_at) as day,
+    COUNT(*) as total
+FROM
+    users
+GROUP BY
+    day
+ORDER BY
+    total DESC
+LIMIT
+    2;
 
 -- instagram challenge #3 IDENTIFIED INACTIVE USERS
+SELECT
+    username,
+    image_url
+FROM
+    users
+    LEFT JOIN photos ON users.id = photos.user_id
+WHERE
+    photos.id IS NULL;
 
-SELECT username, image_url FROM users LEFT JOIN photos ON users.id = photos.user_id
-WHERE photos.id IS NULL;
+-- Instagram CHALLENGE #4 who get the most likes 
+SELECT
+    username,
+    photos.id,
+    photos.image_url,
+    COUNT(*) as total
+FROM
+    photos
+    INNER JOIN likes on likes.photo_id = photos.id
+    INNER JOIN users ON photos.user_id = users.id
+GROUP BY
+    photos.id
+ORDER BY
+    total DESC
+LIMIT
+    1;
